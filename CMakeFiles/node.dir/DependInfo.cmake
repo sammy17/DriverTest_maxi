664d62261f059ba452511659723f75365c22b828
sammy17 <chathura.abeyrathne.lk@gmail.com>
2017-12-18 07:33:48 +0000

    sync

---
+++ CMakeFiles/node.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/debian/DriverTest_maxi/drivers/xbacksub.c" "/home/debian/DriverTest_maxi/CMakeFiles/node.dir/drivers/xbacksub.c.o"
+  "/home/debian/DriverTest_maxi/drivers/xbacksub_linux.c" "/home/debian/DriverTest_maxi/CMakeFiles/node.dir/drivers/xbacksub_linux.c.o"
+  "/home/debian/DriverTest_maxi/drivers/xbacksub_sinit.c" "/home/debian/DriverTest_maxi/CMakeFiles/node.dir/drivers/xbacksub_sinit.c.o"
+  "/home/debian/DriverTest_maxi/drivers/xfeature.c" "/home/debian/DriverTest_maxi/CMakeFiles/node.dir/drivers/xfeature.c.o"
+  "/home/debian/DriverTest_maxi/drivers/xfeature_linux.c" "/home/debian/DriverTest_maxi/CMakeFiles/node.dir/drivers/xfeature_linux.c.o"
+  "/home/debian/DriverTest_maxi/drivers/xfeature_sinit.c" "/home/debian/DriverTest_maxi/CMakeFiles/node.dir/drivers/xfeature_sinit.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/debian/DriverTest_maxi/detection/BGSDetector.cpp" "/home/debian/DriverTest_maxi/CMakeFiles/node.dir/detection/BGSDetector.cpp.o"
+  "/home/debian/DriverTest_maxi/detection/Detector.cpp" "/home/debian/DriverTest_maxi/CMakeFiles/node.dir/detection/Detector.cpp.o"
+  "/home/debian/DriverTest_maxi/detection/NodeClient.cpp" "/home/debian/DriverTest_maxi/CMakeFiles/node.dir/detection/NodeClient.cpp.o"
+  "/home/debian/DriverTest_maxi/maxi.cpp" "/home/debian/DriverTest_maxi/CMakeFiles/node.dir/maxi.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/node.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/node.dir/maxi.cpp.o"
+  "CMakeFiles/node.dir/drivers/xbacksub.c.o"
+  "CMakeFiles/node.dir/drivers/xbacksub_linux.c.o"
+  "CMakeFiles/node.dir/drivers/xbacksub_sinit.c.o"
+  "CMakeFiles/node.dir/drivers/xfeature.c.o"
+  "CMakeFiles/node.dir/drivers/xfeature_linux.c.o"
+  "CMakeFiles/node.dir/drivers/xfeature_sinit.c.o"
+  "CMakeFiles/node.dir/detection/BGSDetector.cpp.o"
+  "CMakeFiles/node.dir/detection/Detector.cpp.o"
+  "CMakeFiles/node.dir/detection/NodeClient.cpp.o"
+  "node.pdb"
+  "node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
